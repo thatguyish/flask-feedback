ab128eddb54f5adee6be88a393ad4d37b71cdf86
thatguyish <ishmell.ramirez@gmail.com>
2021-08-10 14:16:07 -0400

    added heroku datbase support

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',"secretisthis")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///feedback"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',"postgresql:///feedback")
 
 connect_db(app)
 
